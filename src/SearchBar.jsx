f4247d14594e297e320bf3b172a3bc9849f997e3
Marek Vcelak <vcelak.marek@gmail.com>
2022-12-22 01:29:34 +0100

    Update SearchBar.jsx

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./css/SearchBar-styles.css";
 
 const SearchBar = () => {
+  const [searchedUserName, setSearchedUserName] = useState();
+
   return (
     <>
       <div id="search-bar" className="card">
         <div className="search-line">
           <img src="./assets/icon-search.svg" alt="icon-search" />
-          <input type="text" placeholder="Search GitHub username…" />
+          <label htmlFor="search-username">
+            <input
+              type="text"
+              id="search-username"
+              value={searchedUserName}
+              onChange={(e) => {
+                setSearchedUserName(e.target.value);
+              }}
+              placeholder="Search GitHub username…"
+            />
+          </label>
           <p className="hidden">No results</p>
           <button>Search </button>
         </div>
