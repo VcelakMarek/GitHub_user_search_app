00926b4e176f155987c1fdf5b156d4554e664c50
Marek Vcelak <vcelak.marek@gmail.com>
2022-12-23 01:24:59 +0100

    add useEffect

--- src/UserInfo.jsx
+++ src/UserInfo.jsx
@@ -1,6 +1,33 @@
+import { useState, useEffect } from "react";
 import "./css/UserInfo-styles.css";
 
 const UserInfo = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/octocat`);
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        let actualData = await response.json();
+        setData(actualData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div className="card">
       <section className="user-profile">
