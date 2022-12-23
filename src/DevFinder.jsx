0a790546c6436a88426d3fe04d7d783acd65e47a
Marek Včelák <vcelak.marek@gmail.com>
2022-12-23 21:53:37 +0100

    Update DevFinder.jsx

--- src/DevFinder.jsx
+++ src/DevFinder.jsx
@@ -1,17 +1,47 @@
+import { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import UserInfo from "./UserInfo";
 import "./css/DevFinder-styles.css";
 
 const DevFinder = () => {
+  const [darkMode, setDarkMode] = useState();
+
+  const changeColorMode = () => {
+    darkMode ? setDarkMode(false) : setDarkMode(true);
+
+    document.documentElement.style.setProperty("--silver-white", "#141d2f");
+    document.documentElement.style.setProperty("--white", "#1e2a47");
+    document.documentElement.style.setProperty("--font-black", "#ffffff");
+    document.documentElement.style.setProperty("--blue", "#ffffff");
+  };
+
+  useEffect(() => {
+    window.matchMedia("(prefers-color-scheme: dark)")
+      ? setDarkMode(true)
+      : setDarkMode(false);
+  }, []);
+
   return (
     <>
       <header>
         <h1>devfinder</h1>
-        <button className="mode-select">
-          {/* <p>LIGHT</p> */}
-          {/* <img src="./assets/icon-sun.svg" alt="icon-sun" /> */}
-          <p>DARK</p>
-          <img src="./assets/icon-moon.svg" alt="icon-moon" />
+        <button
+          className="mode-select"
+          onClick={() => {
+            changeColorMode();
+          }}
+        >
+          {darkMode ? (
+            <>
+              <p>LIGHT</p>
+              <img src="./assets/icon-sun.svg" alt="icon-sun" />
+            </>
+          ) : (
+            <>
+              <p>DARK</p>
+              <img src="./assets/icon-moon.svg" alt="icon-moon" />
+            </>
+          )}
         </button>
       </header>
       <section className="content">
