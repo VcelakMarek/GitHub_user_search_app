f1af1fbcb70a7e75d004a8b694e507b8c7b59d53
Marek Včelák <vcelak.marek@gmail.com>
2022-12-25 00:19:22 +0100

    Update fetchGitHubUser.js

--- src/fetchGitHubUser.js
+++ src/fetchGitHubUser.js
@@ -1,11 +1,14 @@
 const fetchGitHubUser = async (userName, setUserData) => {
   const res = await fetch(`https://api.github.com/users/${userName}`);
 
-  if (!res.ok) {
+  if (!res.ok && res.status != 404) {
     throw new Error(`This is an HTTP error: The status is ${res.status}`);
+  } else if (res.status === 404) {
+    setUserData(res.status);
+  } else {
+    const json = await res.json();
+    setUserData(json);
   }
-  const json = await res.json();
-  setUserData(json);
 };
 
 export default fetchGitHubUser;
